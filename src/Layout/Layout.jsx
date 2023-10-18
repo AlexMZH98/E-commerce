99f1e7ede863a936b8e67ae93bacb72f0c24a40b
AlexMZH98 <alex.mzhava@gmail.com>
2023-10-18 21:28:31 +0400

    First commit

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+
+import { getCartData } from "../store/CartSlice/CartSlice"
+
+import Header from "../Components/Header/Header"
+import ResponsiveHeader from "../Components/ResponsiveHeader/ResponsiveHeader"
+import Router from "../Router/Router"
+import Footer from "../Components/Footer/Footer"
+
+import "./style.scss"
+import BurgerMenu from "../Components/BurgerMenu/BurgerMenu"
+
+const Layout = () => {
+    const [burgerActive, setBurgerActive] = useState("")
+    const [cartAccessToggle, setCartAccessToggle] = useState('')
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    
+    const userToken = JSON.parse(localStorage.getItem('userToken'))
+    
+    useEffect(() => {
+        if(userToken){
+            dispatch(getCartData())
+            console.log("yes")
+        }
+    },[dispatch,cartAccessToggle])
+
+    const burgerToggle = () => {
+        setBurgerActive((preState) => preState === "" ? "active" : "")
+    }
+    
+    return(
+        <>
+        <div className={`need-auth ${cartAccessToggle}`}>
+            <div className='need-auth-container'>
+                <div>
+                    <h1>You need to be authorized to access your cart</h1>
+                </div>
+                <div className='need-auth-buttnos'>
+                    <div>
+                        <button onClick={() => {
+                            navigate("/login")
+                            setCartAccessToggle('')
+                            setBurgerActive("")
+                        }}>authorize</button>
+                    </div>
+                    <div>
+                        <button onClick={() => {
+                            setCartAccessToggle('')
+                        }}>Cloce</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <BurgerMenu
+        active={burgerActive}
+        changeActive={burgerToggle}
+        changeCartAsseccToggle={setCartAccessToggle}
+        />
+        
+        <div className={`layout ${burgerActive}`}>
+            <Header
+            changeCartAsseccToggle={setCartAccessToggle}
+            />
+            <ResponsiveHeader
+            burgerToggle={burgerToggle}
+            changeCartAsseccToggle={setCartAccessToggle}
+            />
+            <Router/>
+            <Footer/>
+        </div>
+        </>
+    )
+}
+
+export default Layout
